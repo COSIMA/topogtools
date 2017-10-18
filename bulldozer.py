6bbedaff823314c5f37d856a81cf86c98dcbb5f9
Nic Hannah <nic@doubleprecision.com.au>
2017-10-18 15:40:20 +1100

    Test bulldozer by carving out Red Sea, Persian Gulf and Bering Straight.

--- bulldozer.py
+++ bulldozer.py
@@ -4,6 +4,7 @@ import sys
 import os
 import time
 import shutil
+import numpy as np
 import argparse
 import netCDF4 as nc
 
@@ -20,7 +21,7 @@ comma-separated line with the following values:
 
 For example to run on a single point:
 
-echo "112, 246, 0.0, 50.0" | ./bulldozer.py test/topog.nc
+echo -e "112, 246, 0.0, 50.0" | ./bulldozer.py test/topog.nc
 
 To run a whole file:
 
@@ -68,7 +69,7 @@ def apply_change(i, j, orig_depth, new_depth, depth_var, force=False):
 
     # Check that the original depths match
     if depth_var[j, i] != orig_depth:
-        errstr = 'Depth is not as expected'
+        errstr = 'Got {}, expected {} depth'.format(depth_var[j, i], orig_depth)
 
     # See whether we're making a hole
     if making_a_hole(i, j, new_depth, depth_var):
@@ -79,6 +80,58 @@ def apply_change(i, j, orig_depth, new_depth, depth_var, force=False):
 
     return errstr
 
+def bulldoze(new_topog, force=False):
+
+    err = False
+    with nc.Dataset(new_topog, 'r+') as nf:
+        depth = nf.variables['depth']
+
+        for line in sys.stdin.readlines():
+            try:
+                l = line.split(',')
+                i = int(l[0].strip())
+                j = int(l[1].strip())
+                orig_depth = float(l[2].strip())
+                new_depth = float(l[3].strip())
+            except (ValueError, IndexError):
+                print('Error: {} bad stdin input format')
+                err = True
+                break
+
+            errstr = apply_change(i, j, orig_depth, new_depth, depth,
+                                  force=force)
+            if errstr is not None:
+                errstr = errstr + ' at {} {}'.format(l[0], l[1])
+                if force:
+                    print('Warning: ' + errstr, file=sys.stderr)
+                else:
+                    print('Error: ' + errstr, file=sys.stderr)
+                    err = True
+                    break
+
+            # Add to history attribute
+            hist_str = time.ctime(time.time()) + ' : echo ' + \
+                        line.strip() + ' | ' + ' '.join(sys.argv)
+            if 'history' not in nf.ncattrs():
+                nf.history = hist_str
+            else:
+                nf.history = nf.history + " " + hist_str
+
+    return err
+
+def make_ice_mask(mask_filename, topog_filename, num_lons, num_lats):
+
+    with nc.Dataset(topog_filename, 'r') as tf, \
+            nc.Dataset(mask_filename, 'w') as mf:
+        mf.createDimension('nx', num_lons)
+        mf.createDimension('ny', num_lats)
+        mask = mf.createVariable('kmt', 'f8', dimensions=('ny', 'nx'))
+        # CICE uses 0 as masked
+        m = np.zeros_like(mask)
+        m[:] = 0
+        m[np.where(tf.variables['depth'][:] > 0)] = 1
+        mask[:] = m[:]
+
 
 def main():
 
@@ -86,9 +139,11 @@ def main():
     parser.add_argument('orig_topog', help='The topog file to be changed.')
     parser.add_argument('--new_topog', default=None, help="""
                         The new topog file. If not provided the original will
-                        be changed in-place""")
+                        be changed in-place (untested)""")
     parser.add_argument('--force', default=False,
                         help="Force changes, don't exit on a bad change.")
+    parser.add_argument('--ice_mask', default=None,
+                        help="Make ice land-sea mask based on new topography.")
 
     args = parser.parse_args()
 
@@ -110,47 +165,15 @@ def main():
         parser.print_help()
         return 1
 
-    err = False
-    with nc.Dataset(args.new_topog, 'r+') as nf:
-
-        depth = nf.variables['depth']
-
-        for line in sys.stdin.readlines():
-
-            try:
-                l = line.split(',')
-                i = int(l[0].strip())
-                j = int(l[1].strip())
-                orig_depth = float(l[2].strip())
-                new_depth = float(l[3].strip())
-            except (ValueError, IndexError):
-                print('Error: {} bad stdin input format')
-                err = True
-                break
-
-            errstr = apply_change(i, j, orig_depth, new_depth, depth,
-                                  force=args.force)
-            if errstr is not None:
-                errstr = errstr + ' at {} {}'.format(l[0], l[1])
-                if args.force:
-                    print('Warning: ' + errstr, file=sys.stderr)
-                else:
-                    print('Error: ' + errstr, file=sys.stderr)
-                    err = True
-                    break
-
-            if not err:
-                hist_str = time.ctime(time.time()) + ' : echo ' + \
-                            line.strip() + ' | ' + ' '.join(sys.argv)
-                if 'history' not in nf.ncattrs():
-                    nf.history = hist_str
-                else:
-                    nf.history = nf.history + hist_str
-
+    err = bulldoze(args.new_topog, args.force)
     if err and args.new_topog is not None:
-        shutil.remove(args.new_topog)
+        os.remove(args.new_topog)
         return 1
 
+    if args.ice_mask:
+        make_ice_mask(args.ice_mask, args.new_topog, 360, 300)
+
+    return 0
 
 if __name__ == '__main__':
     sys.exit(main())
--- test/test_bulldozer.py
+++ test/test_bulldozer.py
@@ -1,7 +1,37 @@
 
 import os
+import numpy as np
+import netCDF4 as nc
 import pytest
 import subprocess as sp
+
+bering_straight = '112, 246, 0.0, 50.0'
+red_sea = """324, 176, 0.0, 50.0
+             324, 175, 0.0, 50.0
+             323, 177, 0.0, 50.0
+             323, 178, 0.0, 50.0
+             323, 179, 0.0, 50.0
+             323, 180, 0.0, 50.0
+             322, 181, 0.0, 50.0
+             322, 182, 0.0, 50.0
+             322, 183, 0.0, 50.0
+             321, 184, 0.0, 50.0
+             321, 185, 0.0, 50.0
+             320, 186, 0.0, 50.0
+             320, 187, 0.0, 50.0
+             319, 188, 0.0, 50.0
+             319, 189, 0.0, 50.0
+             318, 190, 0.0, 50.0
+             318, 191, 0.0, 50.0
+             317, 192, 0.0, 50.0
+             317, 193, 0.0, 50.0
+             316, 194, 0.0, 50.0"""
+gulf_bay = """335, 191, 0.0, 50.0
+              331, 191, 0.0, 50.0
+              330, 191, 0.0, 50.0
+              329, 192, 0.0, 50.0
+              328, 193, 0.0, 50.0
+              328, 194, 0.0, 50.0"""
 
 class TestBulldozer():
 
@@ -12,22 +42,36 @@ class TestBulldozer():
         bulldozer = os.path.join(my_path, '../', 'bulldozer.py')
         topog = os.path.join(test_data, 'topog.nc')
         new_topog = os.path.join(test_data, 'new_topog.nc')
+        ice_mask = os.path.join(test_data, 'kmt.nc')
+        new_ice_mask = os.path.join(test_data, 'new_kmt.nc')
+
+        # Count the original number of ocean points in the ice mask.
+        with nc.Dataset(ice_mask, 'r') as f:
+            ocean_pts = np.sum(f.variables['kmt'][:])
 
         try:
             os.remove(new_topog)
         except:
             pass
+        try:
+            os.remove(new_ice_mask)
+        except:
+            pass
 
-        cmd = [bulldozer, topog, '--new_topog', new_topog]
+        cmd = [bulldozer, topog, '--new_topog', new_topog,
+               '--ice_mask', new_ice_mask]
         p = sp.Popen(cmd, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
-        stderr, stdout = p.communicate('112, 246, 0.0, 50.0'.encode())
-
-        if stderr != b'':
-            print(stderr)
-        if stdout != b'':
-            print(stdout)
+        # Fix up the Bering Straight, Red Sea and Gulf Bay
+        fixes = bering_straight + '\n' + red_sea + '\n' + gulf_bay
+        stderr, stdout = p.communicate(fixes.encode())
+        assert stderr == b'' and stdout == b''
 
         assert p.returncode == 0
-        assert stderr == b''
-        assert stdout == b''
         assert os.path.exists(new_topog)
+        assert os.path.exists(new_ice_mask)
+
+        # Expect there to be one more ocean point than before.
+        with nc.Dataset(new_ice_mask, 'r') as f:
+            new_ocean_pts = np.sum(f.variables['kmt'][:])
+
+        assert new_ocean_pts == ocean_pts + 27
