9be3b2ce6abd48f77a2ace9eff6b308d3510fef3
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-08-31 09:09:11 +1000

    rename test_one to gen_topo; merge build_test_one.sh into build.sh; improve .gitignore

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+module load intel-compiler
+module load netcdf
 
 ff="-I/$NETCDF_ROOT/include/Intel -L$NETCDF_ROOT/lib/Intel -lnetcdff -lnetcdf"
-#ifort check_nonadvective_mosaic.f90 -o check_nonadvective_mosaic  $ff
+ifort check_nonadvective_mosaic.f90 -o check_nonadvective_mosaic  $ff
 ifort do_partial_cells.f90  -o do_partial_cells $ff
-#ifort fix_nonadvective_mosaic.f90 -o fix_nonadvective_mosaic $ff
-#ifort float_vgrid.f90 -o float_vgrid $ff
-#ifort min_depth.f90 -o min_depth $ff
+ifort fix_nonadvective_mosaic.f90 -o fix_nonadvective_mosaic $ff
+ifort float_vgrid.f90 -o float_vgrid $ff
+ifort min_depth.f90 -o min_depth $ff
+ifort kdtree2.f90 gen_topo.f90 -o gen_topo $ff
 
--- build_test_one.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-module load intel-compiler
-module load netcdf
-ff="-I/$NETCDF_ROOT/include/Intel -L$NETCDF_ROOT/lib/Intel -lnetcdff -lnetcdf"
-ifort kdtree2.f90 test_one.f90 -o test_one $ff
-
