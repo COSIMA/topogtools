04e073269dea6ddc4868c79a5584cd0ecdf19e67
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-11-25 11:03:09 +1100

    fix all .res.nc restarts in fix_ocean_restarts.py

--- fix_ocean_restarts.py
+++ fix_ocean_restarts.py
@@ -7,6 +7,7 @@ import shutil
 import numpy as np
 import netCDF4 as nc
 import multiprocessing as mp
+from glob import glob
 
 """
 If the topog.nc file is changed and there are new wet or dry points then
@@ -15,18 +16,11 @@ creating/updating restarts for the new bathymetry are as follows:
 
 1. Do a short run from rest using the new bathymetry file.
 2. Collate the above.
-3. Run this script
+3. Run this script using the collated outputs from 2. as the template
 
-Presently it only works on the 0.1 deg.
+Presently it only works on the 0.1 deg with 75 levels.
 """
 
-mom_restart_files = ['ocean_age.res.nc', 'ocean_bih_friction.res.nc',
-                     'ocean_density.res.nc', 'ocean_sbc.res.nc',
-                     'ocean_temp_salt.res.nc',
-                     'ocean_velocity_advection.res.nc', 'ocean_barotropic.res.nc',
-                     'ocean_pot_temp.res.nc',
-                     'ocean_frazil.res.nc', 'ocean_thickness.res.nc',
-                     'ocean_velocity.res.nc']
 
 def copy_data(args):
 
@@ -43,18 +37,19 @@ def copy_data(args):
 
             # Copy over level by level to save memory
             print('Processing var {}'.format(var))
-            for l in range(75):
-                in_data = in_fp.variables[var][0, l, :, :]
-                out_data = out_fp.variables[var][0, l, :, :]
+            for level in range(75):
+                in_data = in_fp.variables[var][0, level, :, :]
+                out_data = out_fp.variables[var][0, level, :, :]
                 mask = np.logical_and(np.logical_not(in_data.mask),
                                       np.logical_not(out_data.mask))
                 out_data[np.where(mask)] = in_data[np.where(mask)]
+
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("template_dir",
-                        help="Name of directory containing collated restart files from a run using the new bathymetry.")
+                        help="Name of directory containing collated restart files from a run using the new bathymetry (typically a short run from rest).")
     parser.add_argument("old_dir",
                         help="Name of directory containing collated restart files from a run using the old bathymetry.")
     parser.add_argument("output_dir",
@@ -63,15 +58,17 @@ def main():
                         help="Number of processes to use.")
     args = parser.parse_args()
 
-    template_files = [os.path.join(args.template_dir, f) for f in mom_restart_files]
-    old_files = [os.path.join(args.old_dir, f) for f in mom_restart_files]
-    output_files = [os.path.join(args.output_dir, f) for f in mom_restart_files]
+    template_files = glob(os.path.join(args.template_dir, '*.res.nc'))
+    template_files.sort()
+    old_files = [os.path.join(args.old_dir, os.path.basename(f)) for f in template_files]
+    output_files = [os.path.join(args.output_dir, os.path.basename(f)) for f in template_files]
 
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
 
-    for csf, of in zip(template_files, output_files):
-        shutil.copy(csf, of)
+    for tf, of in zip(template_files, output_files):
+        print('Copying', tf, 'to', of)
+        shutil.copy(tf, of)
 
     pool = mp.Pool(args.nprocs)
     pool.map(copy_data, zip(old_files, output_files))
